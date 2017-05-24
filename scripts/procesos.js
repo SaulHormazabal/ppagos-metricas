8139214fa44fc28a4018d2855e962b08f94c8e1b
Saúl Hormazábal Chávez <saul.hormazabal@unholster.com>
2017-05-24 12:09:32 -0400

    feat: 2 reports

---
+++ scripts/procesos.js
@@ -0,0 +1,57 @@
+var completitud_params = {
+    size: {
+        height: 180,
+    },
+    data: {
+        columns: [
+            ['Completas'  , 0.01, 0.03, 0.05, 0.10, 0.15, 0.22, 0.25, 0.26, 0.30, 0.40, 0.45, 0.50, 0.60, 0.60, 0.60, 0.60, 0.60, 0.60, 0.60, 0.60, 0.60, 0.60, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70, 0.70],
+            ['Incompletas', 0.99, 0.97, 0.95, 0.90, 0.85, 0.78, 0.75, 0.74, 0.70, 0.60, 0.55, 0.50, 0.40, 0.40, 0.40, 0.40, 0.40, 0.40, 0.40, 0.40, 0.40, 0.40, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30],
+        ],
+        type: 'area',
+        order: 'asc',
+        groups: [
+            [
+                'Incompletas',
+                'Completas',
+            ],
+        ],
+    },
+    color: {
+        pattern: [
+            '#1F77B4',
+            '#CCCCCC',
+        ],
+    },
+    padding: {
+        left: 35,
+        right: 5,
+    },
+    grid: {
+        x: {
+            lines: [
+                {value: 5, text: 'Deadline', class: 'stroke-red'},
+            ],
+        },
+    },
+    axis: {
+        x: {
+            padding: {
+                left: 0,
+                right: 0,
+            },
+        },
+        y: {
+            padding: {
+                top: 0,
+            },
+            tick: {
+                format: d3.format('p'),
+            },
+        },
+    },
+};
+
+c3.generate(Object.assign({bindto: '#completitud-acreedores-emisiones'}, completitud_params));
+c3.generate(Object.assign({bindto: '#completitud-acreedores-recepciones'}, completitud_params));
+c3.generate(Object.assign({bindto: '#completitud-acreedores-aceptaciones'}, completitud_params));
+c3.generate(Object.assign({bindto: '#completitud-acreedores-pagos'}, completitud_params));
